193779c83fab4132fb154b15f04df43a9ab2108e
vonEmpalmeOlmos <daniel.m@stormclouds.me>
2022-08-13 19:24:46 +0200

    Adding conditional output for auto-generated key

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output network_entity_id {
 output public_ip {
     value = oci_core_instance.instance0.public_ip
 }
+
+output "auto_generated_ssh_key" {
+    value       = (var.ssh_public_key != "") ? null : tls_private_key.compute_ssh_key[0].public_key_openssh
+    sensitive   = true
+}
